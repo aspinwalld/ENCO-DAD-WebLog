e3bbe2d8fa0db9ef3624f172346d3c12b7846b3f
aspinwalld <di@cflsound.com>
2020-10-14 22:41:44 -0400

    DI Aspinwall 2020/10/14
    Started user-configurable settings and increased code readability.
    DEVELOPMENT UPDATE ONLY -> NON-FUNCTIONAL

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 #### WebLog generates a HTML file based on an ENCO .REP file and uploads the html file via ftp.
 
-In progress. Documentation coming once more progress has been made...
+## Configuration  
+Various WebLog settings can be controlled through a *config.ini* file associated with WebLog.
+
+##### NETWORK  
+WebLog can be triggered via ENCO DCL Commands.  
+**IP**: The IP address of the computer the WebLog server will run on (this is required as many machines running WebLog may have multiple NICs).  
+**Port**: The UDP port for listening for incoming DCLs from the ENCO system. ENCO uses UDP port 2002 for DCL commands, and this port should not be changed under normal circumstances.
+
+##### FTP_UPLOAD  
+WebLog can automatically upload the generated HTML file to a FTP server.  
+**Server**: IP address or URL of FTP server.  
+**Username**: Username credential for FTP server.  
+**Password**: Password credential for FTP server.
+
+##### PLAYLIST  
+**Input**: Path to the PLIST.REP or PLIST2.REP report files on the ENCO. (Note these files must be formatted as notated in the *Playlist Report Formatting* section).  
+> Example: *E:\DAD\Files\PLIST.REP*
+**Output**: Path and filename of the generated html file.  
+> Example: *C:\WebLog\Playlist.html*  
+**Delimeter**: String used as a delimeter value in the Plist.rpg file. For more information, refer to the *Playlist Report Formatting* section.  
+**Hours**: Hours of the day to include in the html playlist report. Comma seperated values, 24 hour clock, leading zeros.   
+> Example: ***09, 10, 11, 12, 13*** *would output the 9:00am, 10:00am, 11:00am, 12:00pm, and 1:00pm hours.*
+
+## Playlist RPG Formatting  
+ENCO reports are generated based on RPG template files. In order for WebLog to properly function, use the included Plist.rpg template.  
+*Note that this template assumes your library is called "CUTS". If your library has a different name, it can be changed in the RPG file.*
--- main.py
+++ main.py
@@ -3,15 +3,99 @@
 import ftplib
 import socket
 import time
+import logging
+from configparser import ConfigParser
 
-ip = 'YOUR_IP_HERE'
-port = 2002
+log = logging.getLogger(__name__)
+log_format = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)d] %(funcName)s [%(levelname)s] %(message)s')
+console_logger = logging.StreamHandler()
+console_logger.setFormatter(log_format)
+log.addHandler(console_logger)
+log.setLevel(logging.DEBUG)
 
-ftp_host = 'YOUR_FTP_SERVER_HERE'
-ftp_user = 'YOUR_FTP_USER_HERE'
-ftp_password = 'YOUR_FTP_PASSWORD_HERE'
+ini = ConfigParser()
 
-input_file = 'E:\DAD\Files\PLIST.REP'
+ini.read('config.ini')
+
+# Read/Validate ip from config.ini
+try:
+    ip = ini['NETWORK']['IP']
+    log.debug(f'IP address, {ip}, read from config.')
+except:
+    log.error('Error reading IP address from config. Attempting to use 127.0.0.1.')
+    ip = '127.0.0.1'
+try:
+    socket.inet_aton(ip)
+    log.debug(f'Successfully validated {ip}.')
+except socket.error:
+    log.error(f'The IP address in the config, {ip}, is not valid. Attempting to use 127.0.0.1.')
+    ip = '127.0.0.1'
+
+# Read / Validate port from config.ini
+try:
+    port = ini['NETWORK']['Port']
+    log.debug(f'UDP port, {port}, read from config.')
+except:
+    log.error('Error reading port from config. Attempting to use ENCO DCL default 2002.')
+    port = 2002
+try:
+    port = int(port)
+    valid_port = port in range(1024, 49151)
+    if valid_port:
+        log.debug(f'Successfully validated {port}.')
+    else:
+        log.error(f'The port in the config, {port}, is outside of the allowed range. Attempting to use ENCO DCL default 2002.')
+        port = 2002
+except:
+    log.error(f'The port in the config, {port}, is invalid. Attempting to use ENCO DCL default 2002.')
+    port = 2002
+
+# Read DCL command string from config.ini
+try:
+    dcl_command = ini['NETWORK']['DCL_Command']
+    log.debug(f'DCL command string, {dcl_command}, read from config.')
+except:
+    log.error('Error reading DCL command string from config. Using WebLog default; MAKELOG+HTML.')
+    dcl_command = 'MAKELOG+HTML'
+
+# Read / Validate FTP server enable state from config.ini
+try:
+    ftp_enabled = ini.getboolean('FTP_UPLOAD', 'Enable')
+    log.debug(f'FTP Uploads {str(ftp_enabled)}')
+except:
+    ftp_enabled = False
+    log.error('Error reading FTP enable state from config. Disabling FTP.')
+
+# Read FTP Server from config.ini
+try:
+    ftp_host = ini['FTP_UPLOAD']['Server']
+    log.debug(f'Server URL, {ftp_host}, read from config.')
+except:
+    ftp_host = 'null'
+    ftp_enabled = False
+    log.error('Error reading FTP Server URL from config. Disabling FTP.')
+
+# Read FTP Username from config.ini
+try:
+    ftp_user = ini['FTP_UPLOAD']['Username']
+    log.debug(f'FTP Username, {ftp_user} read from config.')
+except:
+    ftp_user = 'null'
+    ftp_enabled = False
+    log.error('Error reading FTP Username from config. Disabling FTP.')
+
+# Read FTP Password from config.ini
+try:
+    ftp_password = ini['FTP_UPLOAD']['Password']
+    log.debug('FTP Password read from config.')
+except:
+    ftp_password = 'null'
+    ftp_enabled = False
+    log.error('Error reading FTP Password from config. Disabling FTP.')
+
+
+
+input_file = 'E:\\DAD\\Files\\PLIST.REP'
 output_file = 'index.html'
 
 delimeter = ',%,'
@@ -42,10 +126,26 @@ def GetHour(output, lines, t0, t1, *args, **kwargs):
         vtdj = artist.strip()
         intro = line[3]
         length = line[4]
-        if at0 == t0 and at1 == t1 and vtdj == 'JEN AUSTIN':
-            output.write(f'<tr> <th style="color:#00af00">{airtime}</th> <th style="color:#00af00">{title}</th> <th style="color:#00af00">{artist}</th> <th style="color:#00af00">{intro}</th> <th style="color:#00af00">{length}</th> </tr>')
-        elif at0 == t0 and at1 == t1 and vtdj != 'JEN AUSTIN':
-            output.write(f'<tr> <th>{airtime}</th> <th>{title}</th> <th>{artist}</th> <th>{intro}</th> <th>{length}</th> </tr>')
+        if at0 == t0 and at1 == t1 and vtdj == highlight_keyword:
+            output.write(f"""
+            <tr>
+             <th style="color:#00af00">{airtime}</th>
+             <th style="color:#00af00">{title}</th>
+             <th style="color:#00af00">{artist}</th>
+             <th style="color:#00af00">{intro}</th>
+             <th style="color:#00af00">{length}</th>
+             </tr>
+             """)
+        elif at0 == t0 and at1 == t1 and vtdj != highlight_keyword:
+            output.write(f"""
+            <tr>
+             <th>{airtime}</th>
+             <th>{title}</th>
+             <th>{artist}</th>
+             <th>{intro}</th>
+             <th>{length}</th>
+             </tr>
+             """)
 
 
 def generate_log():
@@ -57,11 +157,40 @@ def generate_log():
     #Create HTML File
     output = open(output_file, 'w')
 
-    output.write('<!DOCTYPE HTML><html><head> <title>MIX 106.9 | Jen Austin</title> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> </head><body> <div class="row"> <div class="col-md-2"></div><div class="col-md-8"> <div class="well"> <h2 style="text-align:center">MIX 106.9 Playlist Log</h2><h4 style="text-align:center">Jen Austin | 10:00 - 14:00</h4>')
+    output.write(f"""
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+     <title>MIX 106.9 | Jen Austin</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+     <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+       <div class="well">
+        <h2 style="text-align:center">{title_text}</h2>
+        <h4 style="text-align:center">{subtitle_text}</h4>
+    """)
 
     GetPlaylist(output, lines)
 
-    output.write(f'</div> <table class="table table-striped"> <thead> <tr> <th>Airtime</th> <th>Title</th> <th>Artist</th> <th>Intro</th> <th>Length</th> </tr></thead> <tbody>')
+    output.write("""
+    </div>
+     <table class="table table-striped">
+      <thead>
+       <tr>
+        <th>Airtime</th>
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Intro</th>
+        <th>Length</th>
+        </tr>
+      </thead>
+      <tbody>""")
 
     t0 = '1'
     t1 = '0'
@@ -78,15 +207,27 @@ def generate_log():
     t1 = '3'
     GetHour(output, lines, t0, t1)
 
-    output.write(' </tbody> </table> <div class="footer"> <h6 style="text-align:center">Created using List2Web for ENCO DAD by D.I. Aspinwall</h6> </div></div><div class="col-md-2"></div><div> </body></html>')
+    output.write("""
+          </tbody>
+        </table>
+        <div class="footer">
+          <h6 style="text-align:center">Created using List2Web for ENCO DAD by D.I. Aspinwall</h6>
+        </div>
+        </div>
+          <div class="col-md-2"></div>
+        </div>
+      </body>
+    </html>
+    """)
     output.close()
 
     print('HTML WebLog Generated.')
-    upload_log()
+    if ftp_enabled:
+        upload_log()
 
 
 def upload_log():
-    print(f'Uploading HTML WebLog to {ftp_host}')
+    print(f'Uploading WebLog to {ftp_host}')
     ftp = ftplib.FTP(ftp_host, ftp_user, ftp_password)
     ftp.encoding = 'utf-8'
     with open(output_file, 'rb') as file:
@@ -109,14 +250,13 @@ while True:
             if not data:
                 break
             
-            if dcl == 'MAKELOG+HTML':
-                print('GENERATING HTML WEBLOG...')
+            if dcl == dcl_command:
+                print('GENERATING WEBLOG...')
                 generate_log()
             else:
-                print(f'Sorry, don\'t know what to do with that... {dcl} is not a valid List2Web DCL.')
+                print(f'Invalid DCL Command sent to WebLog. Ignoring it...')
                 break
     except:
-        print('Something\'s fucked up. Killing myself and trying again...')
+        print('An error has occurred. Attempting to recover...')
         time.sleep(10)
-        print('Okay let\'s do this!')
         break
